c4f13c110d885cd89dd94681a6acfbe1e4dd6591
winfred_zen <869158130@qq.com>
2018-07-22 14:26:38 +0800

    create a repo

---
+++ Example/Pods/Target Support Files/Pods-WZViews_Tests/Pods-WZViews_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WZViews_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WZViews_Tests
+@end
---
+++ Example/Pods/Target Support Files/WZViews/WZViews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WZViews : NSObject
+@end
+@implementation PodsDummy_WZViews
+@end
